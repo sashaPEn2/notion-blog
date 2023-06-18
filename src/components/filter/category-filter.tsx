7da20fef3f49610d0a26ae1bc9466e1c882fd7d3
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2023-06-18 20:50:45 +0300

    Update category-filter.tsx

--- src/components/filter/category-filter.tsx
+++ src/components/filter/category-filter.tsx
@@ -23,7 +23,7 @@ export default function CategoryFilter({
   return (
     <div className="flex flex-col items-center space-y-4 md:items-start">
       <div className="flex items-center">
-        <h2 className="text-xl font-bold">Search by topics</h2>
+        <h2 className="text-xl font-bold">Поиск по темам</h2>
         <button
           onClick={handleClearAll}
           className={`relative -right-10 text-xs ${
@@ -31,7 +31,7 @@ export default function CategoryFilter({
             'pointer-events-none text-gray-300 dark:text-gray-600'
           }`}
         >
-          Clear All
+          Очистить все
         </button>
       </div>
       <div className="flex min-h-[56px] w-[90vw] max-w-[36rem] flex-wrap justify-start gap-y-[0.3rem]">

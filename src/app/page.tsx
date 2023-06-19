735b04a6b1e1aa859810317e9ed332c054fa20a1
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2023-06-19 22:31:15 +0300

    1111

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return <h1 className="mt-12 text-center text-3xl font-bold">Главная</h1>;
   const Content = () => (
     <div>
-      <img src="sashaPEn2/notion-blog/src/app/World Water Day (Banner (Landscape)).png" className={content.car} />
+      <img src="sashaPEn2/notion-blog/src/app/World Water Day (Banner (Landscape)).png"/>
     </div>
   );
 }

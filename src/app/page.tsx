36ba8dda8c30e195c91fd03edbdd3788f309e4b6
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2023-06-19 22:29:45 +0300

    111

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,4 +4,9 @@ export const metadata = {
 
 export default function HomePage() {
   return <h1 className="mt-12 text-center text-3xl font-bold">Главная</h1>;
+  const Content = () => (
+    <div>
+      <img src="sashaPEn2/notion-blog/src/app/World Water Day (Banner (Landscape)).png" className={content.car} />
+    </div>
+  );
 }

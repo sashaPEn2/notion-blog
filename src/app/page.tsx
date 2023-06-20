c29e44141f6b0b86e988446c54ea22469a90265e
sashaPEn2 <113623246+sashaPEn2@users.noreply.github.com>
2023-06-20 21:44:15 +0300

    111

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import React from "react";
+import imageToAdd from "./src/app/11588.png";
+
 export const metadata = {
   title: 'Добро пожаловать | Александр Невдах',
 };
 
 export default function HomePage() {
   return <h1 className="mt-12 text-center text-3xl font-bold">Главная</h1>;
-  const Content = () => (
-    <div>
-      <img src="sashaPEn2/notion-blog/src/app/World Water Day (Banner (Landscape)).png"/>
-    </div>
-  );
+  return <img src={imageToAdd} alt="Image" />;
 }
+
